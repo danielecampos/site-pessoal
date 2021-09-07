afde5d0b95e66f7787071b8dc8b749d94d17dd1b
danielecampos <danielecampos16@gmail.com>
2021-09-07 02:49:08 -0300

    release

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Home from "./components";
 import Resume from "./components/Resume";
-import Navbar from "./components/Navbar";
+//import Navbar from "./components/Navbar";
 import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
 
