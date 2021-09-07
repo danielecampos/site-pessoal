99a87883ec17a71b762cac32df3a3d828a206bd4
danielecampos <danielecampos16@gmail.com>
2021-09-07 01:59:23 -0300

    Updates

---
+++ precache-manifest.eb0a47b8c39bbc8bf9043344f29b14d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b9301b5a9c827bf51da9f63720cafc6",
+    "url": "/site-pessoal/index.html"
+  },
+  {
+    "revision": "9ecb96bc6b3b111c1403",
+    "url": "/site-pessoal/static/css/main.5ef259b3.chunk.css"
+  },
+  {
+    "revision": "ed4dd4de0abc76e6418e",
+    "url": "/site-pessoal/static/js/2.0d4936b4.chunk.js"
+  },
+  {
+    "revision": "bfbf98b39da508f522b8cab91a8e0702",
+    "url": "/site-pessoal/static/js/2.0d4936b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ecb96bc6b3b111c1403",
+    "url": "/site-pessoal/static/js/main.6122e61a.chunk.js"
+  },
+  {
+    "revision": "f49b3e996091bde410ea",
+    "url": "/site-pessoal/static/js/runtime-main.dc6524bf.js"
+  },
+  {
+    "revision": "7c8e5f50725885d51b72b45164274e41",
+    "url": "/site-pessoal/static/media/avatar.7c8e5f50.png"
+  }
+]);
